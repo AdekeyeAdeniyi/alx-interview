d8ff881421e579a1db867009ab995f489c893779
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-01-25 23:04:18 +0100

    [fix :dart:] - update code algorithm

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,5 +8,7 @@ represents a valid UTF-8 encoding
 def validUTF8(data):
     if not data and len(data) <= 0:
         return False
-    result = all([True if val <= 128 else False for val in data])
+    result = all(
+        [True if (val).bit_length() <= 8 else False for val in data]
+        )
     return result
