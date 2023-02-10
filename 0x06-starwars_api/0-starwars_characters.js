08cd2aa196c7856fab74762032c26f6fddbd47f7
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-02-10 13:04:27 +0100

    [feat :dart:] - simple API rquest with Starwar API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,37 @@
+const request = require("request");
+
+const filmId = process.argv[2];
+
+if (!filmId || isNaN(filmId)) {
+  process.exit(1);
+}
+
+const API_ADDRESS = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+const setOption = (url) => {
+    return option ={
+        url: url,
+        headers: {
+            'User-Agent': 'request'
+        }
+    }
+}
+
+
+request(setOption(API_ADDRESS), (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            const {characters} = JSON.parse(body);
+            Requestcharacters(characters);
+        }
+    }
+)
+
+const Requestcharacters = (characters) =>{
+    [...characters].forEach(url =>{
+        request(setOption(url), (error, response, body)=>{
+            if (!error && response.statusCode == 200) {
+                const {name} = JSON.parse(body);
+                console.log(name);
+            }
+        })
+    } )
+}
